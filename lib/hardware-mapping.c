050e64634272cdf62e3373afaee8ce2f82cb6fe4
Alex Vecchio <alevecchio@users.noreply.github.com>
2022-01-22 15:18:11 -0300

    Update hardware-mapping.c
    Updated hardware mapping to use only chains 1 and 2 as chains 0 and 1 to keep SPI0 pins free.

--- lib/hardware-mapping.c
+++ lib/hardware-mapping.c
@@ -212,6 +212,52 @@ struct HardwareMapping matrix_hardware_mappings[] = {
     .p0_g2         = GPIO_BIT(24),
     .p0_b2         = GPIO_BIT(25),
   },
+  
+    /* The intention is to change the GPIO mappings to use only chains 1 and 2
+     * and to keep the SPI pins free.
+     */
+  {
+    .name          = "regular",
+
+    .output_enable = GPIO_BIT(18),
+    .clock         = GPIO_BIT(17),
+    .strobe        = GPIO_BIT(4),
+
+    /* Address lines */
+    .a             = GPIO_BIT(22),
+    .b             = GPIO_BIT(23),
+    .c             = GPIO_BIT(24),
+    .d             = GPIO_BIT(25),
+    .e             = GPIO_BIT(15),  /* RxD kept free unless 1:64 */
+
+    /* Parallel chain 0, RGB for both sub-panels */
+    /* Chain 0 pins commented to keep SPI pins free. */  
+      
+    // .p0_r1         = GPIO_BIT(11),  /* masks: SPI0_SCKL  */
+    // .p0_g1         = GPIO_BIT(27),  /* Not on RPi1, Rev1; use "regular-pi1" instead */
+    // .p0_b1         = GPIO_BIT(7),   /* masks: SPI0_CE1   */
+    // .p0_r2         = GPIO_BIT(8),   /* masks: SPI0_CE0   */
+    // .p0_g2         = GPIO_BIT(9),   /* masks: SPI0_MISO  */
+    // .p0_b2         = GPIO_BIT(10),  /* masks: SPI0_MOSI  */
+
+    /* All the following are only available with 40 GPIP pins, on A+/B+/Pi2,3 */
+    /* Chain 0 used with Chain 1 pins on the board */
+    .p0_r1         = GPIO_BIT(12),
+    .p0_g1         = GPIO_BIT(5),
+    .p0_b1         = GPIO_BIT(6),
+    .p0_r2         = GPIO_BIT(19),
+    .p0_g2         = GPIO_BIT(13),
+    .p0_b2         = GPIO_BIT(20),
+
+    /* Chain 1 using Chain 2 pins on the board*/
+    .p1_r1         = GPIO_BIT(14), /* masks TxD when parallel=3 */
+    .p1_g1         = GPIO_BIT(2),  /* masks SCL when parallel=3 */
+    .p1_b1         = GPIO_BIT(3),  /* masks SDA when parallel=3 */
+    .p1_r2         = GPIO_BIT(26),
+    .p1_g2         = GPIO_BIT(16),
+    .p1_b2         = GPIO_BIT(21),
+  },
+
 
 #ifdef ENABLE_WIDE_GPIO_COMPUTE_MODULE
   /*
